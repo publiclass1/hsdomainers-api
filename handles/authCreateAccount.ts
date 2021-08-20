5f297ac24bda64cd524b7de3c2594abee3b705ee
ralph menguito <publiclass1@gmail.com>
2021-08-21 03:14:41 +0800

    add account endpoint

---
+++ handles/authCreateAccount.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express"
+import { serialize } from "superjson"
+import prismaClient from "../lib/primaClient"
+
+export default async function authCreateAccount(req: Request, res: Response) {
+
+  const {
+    compoundId,
+    userId,
+    providerType,
+    providerId,
+    providerAccountId,
+    refreshToken,
+    accessToken,
+    accessTokenExpires,
+  } = req.body
+
+  try {
+    const data = await prismaClient.account.create({
+      data: {
+        compoundId,
+        userId,
+        providerType,
+        providerId,
+        providerAccountId,
+        refreshToken,
+        accessToken,
+        accessTokenExpires,
+      }
+    })
+
+    res.json(serialize(data).json)
+  } catch (e) {
+    res.status(422).end()
+  }
+}
---
+++ handles/authGetAccount.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express"
+
+export default async function authGetAccount(req: Request, res: Response) {
+
+
+}
